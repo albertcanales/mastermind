4e270c585f9580f0c1b8c5c59f538d5e0c0de9d5
Albert Canales Ros <albertcanalesros@gmail.com>
2023-04-09 16:58:15 +0200

    [Persistance] Created ControladorPersistencia

---
+++ FONTS/src/main/java/persistance/ControladorPersistencia.java
@@ -0,0 +1,35 @@
+package persistance;
+
+import java.time.Duration;
+import java.util.List;
+
+public class ControladorPersistencia {
+
+    public Integer getNivellDificultatPartidaGuardada() {
+        return null;
+    }
+
+    public List<List<Integer>> getIntentsPartidaGuardada() {
+        return null;
+    }
+
+    public List<List<Integer>> getFeedbackPartidaGuardada() {
+        return null;
+    }
+
+    public List<Integer> getSolucioPartidaGuardada() {
+        return null;
+    }
+
+    public Boolean isBreakerPartidaGuardada() {
+        return null;
+    }
+
+    public Boolean existsPartidaGuardada() {
+        return null;
+    }
+
+    public Duration getTempsPasrtidaGuardada() {
+        return null;
+    }
+}
