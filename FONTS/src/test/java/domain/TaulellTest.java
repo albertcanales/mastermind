e0f2e93ce8d924df0d723989183487634d248c5f
Arnau Valls Fusté <vallsfustearnau@gmail.com>
2023-04-11 23:23:53 +0200

    [General] add mockito to gradle

--- FONTS/src/test/java/domain/TaulellTest.java
+++ FONTS/src/test/java/domain/TaulellTest.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 public class TaulellTest {
     //TODO assertThrows d'invalidListSize
