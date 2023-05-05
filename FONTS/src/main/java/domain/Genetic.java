c8aebf95daca487066a43220c53103966fbda5c6
Mar Gonzàlez Català <mar.gonzalez.catala@gmail.com>
2023-05-05 20:59:00 +0200

    [Domain] primera versió genetic amb testing

--- FONTS/src/main/java/domain/Genetic.java
+++ FONTS/src/main/java/domain/Genetic.java
@@ -1,6 +1,10 @@
 package domain;
 
-import java.util.List;
+import domain.exceptions.DomainException;
+import domain.exceptions.InvalidEnumValueException;
+import domain.exceptions.InvalidNumBolesException;
+
+import java.util.*;
 
 /**
  * Contenidor de les funcionalitats d'un BotBreaker que empra un algorisme genetic.
@@ -9,14 +13,135 @@ import java.util.List;
  */
 class Genetic extends BotBreaker {
 
+    private Integer numboles;
+    private HashSet<Integer> population;
+
+    /**
+     * Donades dues llistes retorna el nombre de boles amb color i posició coincidents.
+     * @param list1 primera llista  a comparar.
+     * @param list2 segona llista a comparar.
+     * @return Nombre de boles amb color i posició coincidents.
+     */
+    private Integer compareTwoSequencesBlack(ArrayList<Integer> list1, ArrayList<Integer> list2){
+        Integer count = 0;
+        for (int it = 0; it < this.numboles; ++it){
+            if (Objects.equals(list1.get(it), list2.get(it))){
+                count++;
+            }
+        }
+        return count;
+    }
+
+    /**
+     * Genera una població inicial de 128 seqüències.
+     */
+    private void getInitialPopulation() {
+        population = new HashSet<>();
+        Random random = new Random();
+        while (population.size() < 128) {
+            Integer sequencia = 0;
+            for (int i = 0; i < 4; i++) {
+                Integer bola = random.nextInt(6) + 1;
+                sequencia = sequencia*10 + bola;
+            }
+            population.add(sequencia);
+        }
+    }
+
+    /**
+     * Donat un enter el converteix en un vector de dígits
+     * @return Vector de dígits de l'enter.
+     */
+    private ArrayList<Integer> getSequence(Integer intToTranslate){
+        ArrayList<Integer> digits = new ArrayList<>();
+        for (int it = 0; it < this.numboles; it++){
+            digits.add(0,intToTranslate%10);
+            intToTranslate = intToTranslate/10;
+        }
+        return digits;
+    }
+
+    /**
+     * Combina dues seqüències per obtenir-ne una tercera que és combinació de les dues anteriors.
+     * @param seq1
+     * @param seq2
+     * @return seqüència combinació
+     */
+    private Integer combineTwoSequences(Integer seq1, Integer seq2) {
+        return (seq1/100)*100+seq2%100;
+    }
+
+    /**
+     * Aplica un cop l'operador genetic a la població actual per obtenir la següent generació.
+     */
+    private void getNextGeneration() {
+        HashSet<Integer> aux = new HashSet<>();
+        Iterator<Integer> iterator = population.iterator();
+        while (iterator.hasNext()){
+            int seq1 = iterator.next();
+            if (iterator.hasNext()) {
+                int seq2 = iterator.next();
+                int seq = combineTwoSequences(seq1,seq2);
+                aux.add(seq);
+            }
+        }
+        population.clear();
+        for (Integer seq : aux){
+            population.add(seq);
+        }
+    }
+
+    /**
+     * Després d'aplicar uns quants cops l'operador genetic obté un candidat a solució.
+     *
+     * @return Candidat a solució.
+     */
+    private ArrayList<Integer> geneticCandidate(){
+        getInitialPopulation();
+        while (population.size() > 1){
+            getNextGeneration();
+        }
+        Integer chosen = population.iterator().next();
+        ArrayList<Integer> candidate = getSequence(chosen);
+        return candidate;
+    }
+
     /**
      * Donada una solució genera la llista d'intents fins a arribar a ella si utilitzem un algorisme genetic.
      *
-     * @param solution
+     * @param sol
      * @return Llista d'intents.
      */
-    public List<List<Integer>> solve(List<Integer> solution) {
-        return null;
+    public List<List<Integer>> solve(List<Integer> sol) throws DomainException {
+        ArrayList<Integer> solution = new ArrayList<>(sol);
+
+        if (solution.size() != 4) {
+            throw new InvalidNumBolesException(solution.size(),4);
+        }
+
+        for (Integer bola : solution){
+            if (bola < 1 || bola > Bola.numColors()){
+                throw new InvalidEnumValueException("Bola",bola.toString());
+            }
+        }
+
+        numboles = solution.size();
+
+        ArrayList<ArrayList<Integer>> guesses = new ArrayList<>();
+        ArrayList<Integer> currentGuess = new ArrayList<>();
+        boolean won = false;
+
+        while (!won){
+            currentGuess = geneticCandidate();
+
+            guesses.add(currentGuess);
+
+            int black = compareTwoSequencesBlack(currentGuess,solution);
+            if (black == 4) {
+                won = true;
+            }
+        }
+        return new ArrayList<>(guesses);
     }
 
     /**
@@ -25,5 +150,21 @@ class Genetic extends BotBreaker {
     @Override
     TipusAlgorisme getTipusAlgorisme() {
         return TipusAlgorisme.GENETIC;
+    }
+    public static void main(String[] args) throws Exception {
+        List<Integer> solution = new ArrayList<Integer>() {{
+            add(6);
+            add(5);
+            add(4);
+            add(6);
+        }};
+        List<List<Integer>> sortidaFiveGuess = (new Genetic()).solve(solution);
+
+        for (int i = 0; i < sortidaFiveGuess.size(); i++){
+            for (int j = 0; j < sortidaFiveGuess.get(i).size(); j++){
+                System.out.println("Al torn " + i + " bola " + j + " és :" + sortidaFiveGuess.get(i).get(j));
+            }
+            System.out.println();
+        }
     }
 }
---
+++ FONTS/src/test/java/domain/GeneticTests.java
@@ -0,0 +1,69 @@
+package domain;
+
+import domain.exceptions.DomainException;
+import domain.exceptions.InvalidEnumValueException;
+import domain.exceptions.InvalidNumBolesException;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.util.*;
+
+import static org.junit.Assert.*;
+public class GeneticTests {
+    static ArrayList<ArrayList<Integer>> sequencies;
+
+    @BeforeClass
+    public static void beforeClass() {
+        sequencies = new ArrayList<>();
+        Random rand = new Random();
+        for (int it = 0; it < 50; it++){
+            int bola1 = rand.nextInt(Bola.numColors()) + 1;
+            int bola2 = rand.nextInt(Bola.numColors()) + 1;
+            int bola3 = rand.nextInt(Bola.numColors()) + 1;
+            int bola4 = rand.nextInt(Bola.numColors()) + 1;
+            Integer[] seq = {bola1, bola2, bola3, bola4};
+            ArrayList<Integer> sequence = new ArrayList<>(Arrays.asList(seq));
+            sequencies.add(sequence);
+        }
+    }
+
+    @Test
+    public void solve() throws DomainException {
+        BotBreaker genetic = new Genetic();
+        for (ArrayList<Integer> sequencia : sequencies){
+            List<List<Integer>> guesses = genetic.solve(sequencia);
+            int numberOfGuesses = guesses.size();
+            assertEquals("Solució",sequencia,guesses.get(numberOfGuesses-1));
+        }
+    }
+
+    @Test
+    public void solveWrongSizeOfSolution() {
+        BotBreaker genetic = new Genetic();
+        Integer[] sol = {1,1,2,3,4};
+        ArrayList<Integer> solution = new ArrayList<>(Arrays.asList(sol));
+        assertThrows(InvalidNumBolesException.class, () -> genetic.solve(solution));
+    }
+
+    @Test
+    public void solveWrongSolution() {
+        BotBreaker genetic = new Genetic();
+        Integer[] sol1 = {-1,1,2,3};
+        ArrayList<Integer> solution1 = new ArrayList<>(Arrays.asList(sol1));
+        assertThrows(InvalidEnumValueException.class, () -> genetic.solve(solution1));
+        Integer[] sol2 = {1,1,7,3};
+        ArrayList<Integer> solution2 = new ArrayList<>(Arrays.asList(sol2));
+        assertThrows(InvalidEnumValueException.class, () -> genetic.solve(solution2));
+    }
+
+    @Test
+    public void getTipusAlgorisme() {
+        BotBreaker genetic = new Genetic();
+        assertEquals(TipusAlgorisme.GENETIC,genetic.getTipusAlgorisme());
+    }
+
+    public static void main(String[] args) {
+        TestRunner.runTestClass(GeneticTests.class);
+    }
+}
+
